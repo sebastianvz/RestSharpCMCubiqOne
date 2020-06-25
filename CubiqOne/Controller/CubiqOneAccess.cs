1a9f1dc65d436332f5fff59b599eb3cb46a46ff2
Sebastián Velez <devsebastianvz@gmail.com>
2020-06-25 13:21:52 -0500

    base files

---
+++ CubiqOne/Controller/CubiqOneAccess.cs
@@ -0,0 +1,39 @@
+﻿using CubiqOneAcces.Helpers;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubiqOne.Controller
+{
+    public class CubiqOneAccess
+    {
+        public IRestResponse AccessCubiqOne(string resource, string IpAddress, string method, object parameters = null)
+        {
+            var recivemethod = (method.ToLower().Equals("post")) ? Method.POST : Method.GET;
+            IRestResponse resp;
+            //RestClient call request
+            RestClient client = new RestClient();
+            client.ReadWriteTimeout = 1000;
+            client.Timeout = 10000;
+            client.BaseUrl = new Uri("http://"+ IpAddress +"/cubiqMiddlewareApi/api/");
+            //RestRequest has the whole stuff to consume API
+            RestRequest request = new RestRequest(recivemethod);
+            request.Parameters.Clear();
+            request.Resource = resource;
+            request.RequestFormat = DataFormat.Json;
+            if (parameters != null)
+            {
+                request.AddHeader("Accept", "application/json charset=utf-8");
+                var json = JsonConvert.SerializeObject(parameters);
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+            }
+            var sendIt = Utilities.doRequest(request, client);
+            return sendIt;
+        }
+    }
+}
+
---
+++ UnitTest/Testing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CubiqManager.Controller;
+using CubiqOne.Controller;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Testing
+    {
+        [TestMethod]
+        public void TestMethodCubiqManagerAccess()
+        {
+            CubiqManagerAccess test = new CubiqManagerAccess();
+            string resource = "externalapi/getMachines";
+            string apiKey = "";
+            string method = "get";
+            test.AccessCM(resource, apiKey, method);
+        }
+
+        [TestMethod]
+        public void TestMethodCubiqOneAccess()
+        {
+            CubiqOneAccess test = new CubiqOneAccess();
+            string resource = "getConfigAll";
+            string method = "get";
+            string ipAddress = "10.8.0.18";
+            test.AccessCubiqOne(resource, ipAddress, method);
+        }
+    }
+
+}
